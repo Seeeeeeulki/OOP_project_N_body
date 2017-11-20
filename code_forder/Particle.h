3833c239edb6d4b533ed94cf67336894ad812b2e
Jeung DeokGi <31835663+SawamuraSpencerEriri@users.noreply.github.com>
2017-11-20 11:07:08 +0900

    Update Particle.h

--- code_forder/Particle.h
+++ code_forder/Particle.h
@@ -3,8 +3,8 @@
 class particle			//N_body_simulation에서 particle을 표현
 {
 protected:
-	double location[2]; //particle의 위치 [x,y]
-	double velocity[2]; //particle의 속도 [x,y]
+	double location[2]; 	//particle의 위치 [x,y]
+	double velocity[2]; 	//particle의 속도 [x,y]
 	double mass;		//particle이 가진 질량
 	bool movavle;		//particle의 움직임 여부를 결정하는 boolean값
 
