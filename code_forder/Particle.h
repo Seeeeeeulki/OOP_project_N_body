5b01067001efc7e3e56076c416409da482a3e12a
virgil2154 <33818338+virgil2154@users.noreply.github.com>
2017-12-03 15:48:31 +0900

    Update Particle.h
    영어 주석 변경

--- code_forder/Particle.h
+++ code_forder/Particle.h
@@ -2,16 +2,16 @@
 #include <iostream>
 #include <String>
 
-class particle			//N_body_simulation에서 particle을 표현
+class particle			//class of 'particle' in N_body_simulation
 {
 public:
-	double location[2]; 	//particle의 위치 [x,y]
-	double velocity[2]; 	//particle의 속도 [x,y]
-	double mass;			//particle이 가진 질량
-	bool fixed = false;			//particle의 움직임 여부를 결정하는 boolean값, 초기값은 false
-	std::string particle_name;	//particle 이름
+	double location[2]; 	//particle's location [x,y]
+	double velocity[2]; 	//particle's velocity [x,y]
+	double mass;			//particle's mass
+	bool fixed = false;			//boolean value that determine particle's movement, initial value is false
+	std::string particle_name;	//particle's name
 
 
-	particle(std::string name, double mass, double x, double y, double v_x, double v_y); //particle 클래스 변수 초기화 하는 함수
-	void Print_particle(); //particle object의 정보를 출력하는 함수
+	particle(std::string name, double mass, double x, double y, double v_x, double v_y); //particle class value initialize
+	void Print_particle(); //print particle object's information
 };
